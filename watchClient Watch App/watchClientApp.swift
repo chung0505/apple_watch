2dbd8365043325e1bc81240589db281139222114
Chung <kevin506059@gmail.com>
2023-08-28 00:38:50 +0800

    Initial Commit

---
+++ watchClient Watch App/watchClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchClientApp.swift
+//  watchClient Watch App
+//
+//  Created by 李致中 on 2023/8/28.
+//
+
+import SwiftUI
+
+@main
+struct watchClient_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
